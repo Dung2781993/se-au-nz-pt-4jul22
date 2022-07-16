4647ca74fd38c5ab1e227014a6326c763bc68a8a
JasonLe <Dung2781993@gmail.com>
2022-07-16 11:12:06 +1000

    Update method for training 16_07_2022

---
+++ 16_07_2022/helloword.js
@@ -0,0 +1,25 @@
+let plus = (a, b) => {
+    return a + b; 
+}
+
+let minus = (a, b) => {
+    return a - b; 
+}
+
+let division = (a, b) => {
+    return a / b; 
+}
+
+let multiplicity = (a, b) => {
+    return a * b; 
+}
+
+let concatName = (name) => {
+    return `Hello ${name}`; 
+}
+
+
+
+let result = concatName("Test");
+
+console.log(result);
