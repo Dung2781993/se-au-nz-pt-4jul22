ea1dab96585648ff05d4f67258f3117563c5b797
JasonLe <Dung2781993@gmail.com>
2022-07-30 11:19:15 +1000

    Update content

--- 16_07_2022/server.js
+++ 16_07_2022/server.js
@@ -1,4 +1,15 @@
-var Sentiment = require('sentiment');
-var sentiment = new Sentiment();
-var result = sentiment.analyze('Cats are stupid.');
-console.dir(result);
+const express = require("express");
+const app = express();
+
+let concatString = (name) => {
+  let result = `Hello {name}`;
+  console.log
+  return result;
+};
+
+app.get("/", function (req, res) {
+  res.send(concatString("Test"));
+});
+
+//http://localhost:3000/ : Listen on port 3000
+app.listen(3000);
