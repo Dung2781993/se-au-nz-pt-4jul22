6fd2c4a982e2de92dcd8e23ed063985af0c76e75
JasonLe <Dung2781993@gmail.com>
2022-08-13 16:50:58 +1000

    Javascripit intemidate

---
+++ 10_08_2022/function.js
@@ -0,0 +1,66 @@
+let list = [
+  {
+    firstName: "Kiran1",
+    lastName: "Kumar",
+    salary: 25000,
+  },
+  {
+    firstName: "Mohan1",
+    lastName: "Srivastav",
+    salary: 20000,
+  },
+  {
+    firstName: "Mary1",
+    lastName: "Kom",
+    salary: 23000,
+  },
+  {
+    firstName: "Kiran2",
+    lastName: "Kumar",
+    salary: 28000,
+  },
+  {
+    firstName: "Mohan2",
+    lastName: "Srivastav",
+    salary: 30000,
+  },
+  {
+    firstName: "Mary2",
+    lastName: "Kom",
+    salary: 43000,
+  },
+];
+
+//CHeck the name exist or name
+/** 
+ * Get name from input (character or full name)
+*/
+let checkExistedName = (name) => {
+    //Loop through the list and check it is existed in the first or last name 
+
+    return {
+        'existed': true,
+        'numberOfProfile': 2
+    }
+}
+
+let deleteHighest = () => {
+    
+}
+
+
+
+
+
+
+// function lookup(num, prop) {
+//   for (var i = 0; i < 3; i++) {
+//     if (num == i) {
+//       document.getElementById("demo").innerHTML = list[num][prop];
+//       break;
+//     } else {
+//       document.getElementById("demo").innerHTML =
+//         num + " " + i + " " + "No such contact found";
+//     }
+//   }
+// }
